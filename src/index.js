b2e0e69fe9b0a6ad4b83e102b7ec11703762b328
Tristan Shelton <tristan.shelton@aeseducation.com>
2018-12-14 16:28:47 -0500

    Improve plugin

--- src/index.js
+++ src/index.js
@@ -1,41 +1,46 @@
 import Store from 'Store'
-import { lookupDescriptor, resolveParam } from 'util'
+import { resolveParam } from 'util'
 
-function install(Vue, options) {
+function install(Vue, pluginOptions) {
   Vue.mixin({
     beforeCreate() {
       const options = this.$options
 
       if (!options.parent && options.store) {
-        if (!(options.store instanceof Store)) {
-          throw new Error('provided store is not a Rivue store')
+        if (DEVELOPMENT) {
+          if (!(options.store instanceof Store)) {
+            throw new Error('provided store is not a Rivue store')
+          }
         }
 
         this.$store = options.store
       }
 
-      if (options.parent && options.parent.$store) this.$store = options.parent.$store
-
-      // Init
-      const store = this.$store
-      const init = name => typeof options[name] === 'function' ? options[name]() : options[name] || {}
-
-      options.data = init('data')
-      options.computed = init('computed')
-      options.methods = init('methods')
+      if (options.parent && options.parent.$store) {
+        this.$store = options.parent.$store
+      }
 
       if (options.state) {
-        const map = resolveParam(store, options.state)
-
+        const map = resolveParam(this.$store, options.state)
+        const componentName = this.name || this.constructor.name
+  
         // TODO: verification
-
-        map.forEach(({ parent, name }) => {
-          options.computed[name] = {
-            get: function() {
-              return parent[name]
-            },
-            set: function(val) {
-              parent[name] = val
+  
+        if (!this.$options.computed) this.$options.computed = {}
+        map.forEach(({ parent, name, value }) => {
+          if (value === undefined) {
+            if (DEVELOPMENT) {
+              console.warn(`Store '${parentName}' doesn't provide state '${name}' in <${componentName}>`)
+            }
+          }
+          else {
+            this.$options.computed[name] = {
+              get: function () {
+                return parent[name]
+              },
+              set: function (val) {
+                parent[name] = val
+              }
             }
           }
         })
@@ -44,23 +49,18 @@ function install(Vue, options) {
     created() {
       const options = this.$options
       const store = this.$store
-
-      if (options.stores) {
-        const map = resolveParam(store, options.stores)
-
-        // TODO: verification
-
-        map.forEach(({ name, value }) => {
-          this[name] = value
-        })
-      }
+      const componentName = this.name || this.constructor.name
 
       if (options.actions) {
         const map = resolveParam(store, options.actions)
 
-        // TODO: verification
-
-        map.forEach(({ parent, name, value }) => {
+        map.forEach(({ parentName, name, value }) => {
+          if (DEVELOPMENT) {
+            if (!value || typeof value !== 'function') {
+              console.warn(`Store '${parentName}' doesn't provide an action '${name}' in <${componentName}>`)
+            }
+          }
+        
           this[name] = value
         })
       }
